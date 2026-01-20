6095fc460c69cbfa312960bfa14551a9c10da342
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-01-20 10:50:45 +0530

    created regsiter endpoint with otp verification

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
+import { verifyOtp } from "../utils/otpStore";
+
+export default async function registerUser(req: Request, res: Response) {
+  try {
+    const { name, phone, otp } = req.body;
+
+    // -- -- -- -- -- validate phone -- -- -- -- --
+    if (!phone) {
+      return res.status(400).json({ message: "Phone is required" });
+    }
+
+    // -- -- -- -- -- validate otp -- -- -- -- --
+    if (!otp) {
+      return res.status(400).json({ message: "OTP is required" });
+    }
+
+    if (verifyOtp(phone, otp) === false) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    // -- -- -- -- -- check if user exists -- -- -- -- --
+    const user = await prisma.user.findUnique({
+      where: {
+        phone,
+      },
+    });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // -- -- -- -- -- sanitize name -- -- -- -- --
+    const finalName =
+      typeof name === "string" && name.trim().length > 0
+        ? name.trim()
+        : "world";
+
+    // -- create the user --
+    const newUser = await prisma.user.create({
+      data: {
+        name: finalName,
+        phone,
+      },
+    });
+
+    return res.status(201).json({
+      message: "User created succesfully",
+      data: { user: { name: newUser.name, phone: newUser.phone } },
+    });
+  } catch (error) {
+    console.log("AUTH_REGISTER_ERROR:" + error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,8 @@ export function startServer() {
 
   app.use("/health", (_, res) => res.send("OK"));
 
+  app.use("/auth", require("../routes/auth.route"));
+
   const PORT = Number(process.env.PORT) || 5000;
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
