20221610f529be75efd6b7b1dbd8147bfea3d47e
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-01-23 09:27:20 +0530

    created get menu function with filters

--- server/src/controllers/menu.controller.ts
+++ server/src/controllers/menu.controller.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { prisma } from "../lib/prisma";
 import { AuthenticatedRequest } from "../types/auth";
+import { FoodType } from "@prisma/client";
 
 export async function createMenuItem(req: AuthenticatedRequest, res: Response) {
   try {
@@ -29,6 +30,31 @@ export async function createMenuItem(req: AuthenticatedRequest, res: Response) {
       .json({ message: "Menu item created successfully", data: { item } });
   } catch (error) {
     console.log("MENU_ITEM_CREATE_ERROR", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function getMenu(req: AuthenticatedRequest, res: Response) {
+  try {
+    const { categoryId, foodType, search } = req.query;
+
+    const items = await prisma.menuItem.findMany({
+      where: {
+        isAvailable: true,
+        ...(categoryId && { categoryId: String(categoryId) }),
+        ...(foodType && { foodType: foodType as any }),
+        ...(search && {
+          name: { contains: String(search).trim(), mode: "insensitive" },
+        }),
+      },
+      include: { category: true },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Fetched all menu items", data: { items } });
+  } catch (error) {
+    console.log("MENU_GET_ERROR", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
--- server/src/routes/menu.route.ts
+++ server/src/routes/menu.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createMenuItem } from "../controllers/menu.controller";
+import { createMenuItem, getMenu } from "../controllers/menu.controller";
 
 const router = Router();
 
+router.get("/", getMenu);
 router.post("/create", createMenuItem);
 
 export default router;
